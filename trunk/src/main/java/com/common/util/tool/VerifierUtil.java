90f84f257f61ea739a185694ccb93114e0cb5b0b
guillermomazzali@gmail.com <guillermomazzali@gmail.com@68265a98-090c-cc5e-7057-9da8e80d0a77>
2014-03-06 18:24:22 +0000

    Creación de la utilería de los Strings.

--- trunk/src/main/java/com/common/util/tool/VerifierUtil.java
+++ trunk/src/main/java/com/common/util/tool/VerifierUtil.java
@@ -1,7 +1,6 @@
 package com.common.util.tool;
 
 import java.io.Serializable;
-import java.util.regex.Pattern;
 
 /**
  * La clase encargada de verificar que los campos de entrada de una aplicación tenga los valores correctos y que no sean cualquier cosa.
@@ -11,61 +10,6 @@ import java.util.regex.Pattern;
  * @version 1.0
  */
 public class VerifierUtil {
-
-	/**
-	 * Se encarga de verificar que una cadena de caracteres este vacia. Retorna <i>true</i> en caso de que la cadena recibida sea nula.
-	 * 
-	 * <ul>
-	 * <li>VerifierUtil.isEmpty(null) = true</li>
-	 * <li>VerifierUtil.isEmpty("") = true</li>
-	 * <li>VerifierUtil.isEmpty(" ") = false</li>
-	 * <li>VerifierUtil.isEmpty("bob") = false</li>
-	 * <li>VerifierUtil.isEmpty(" bob ") = false</li>
-	 * </ul>
-	 * 
-	 * @param string
-	 *            La cadena que vamos a verificar.
-	 * @return <i>true</i> en caso de que la cadena recibida sea nula o este vacia completamente, en caso contrario retorna <i>false</i>.
-	 */
-	public static boolean isEmpty(String string) {
-		return string != null ? string.isEmpty() : true;
-	}
-
-	/**
-	 * Se encarga de verificar que una cadena de caracteres este compuesta solo de espacios en blanco. Retorna <i>true</i> en caso de que la cadena
-	 * recibida sea nula.
-	 * 
-	 * <ul>
-	 * <li>VerifierUtil.isBlank(null) = true</li>
-	 * <li>VerifierUtil.isBlank("") = true</li>
-	 * <li>VerifierUtil.isBlank(" ") = true</li>
-	 * <li>VerifierUtil.isBlank("bob") = false</li>
-	 * <li>VerifierUtil.isBlank(" bob ") = false</li>
-	 * </ul>
-	 * 
-	 * @param string
-	 *            La cadena que vamos a verificar.
-	 * @return <i>true</i> en caso de que la cadena recibida sea nula o este compuesta solo de espacios en blanco, en caso contrario retorna
-	 *         <i>false</i>.
-	 */
-	public static boolean isBlank(String string) {
-		return string != null ? string.trim().isEmpty() : true;
-	}
-
-	/**
-	 * Se encarga de validar que una cadena de caracteres recibida coincida con un patrón de validación.
-	 * 
-	 * @see PatternUtil
-	 * 
-	 * @param string
-	 *            La cadena que vamos a validar.
-	 * @param pattern
-	 *            El patrón con el que vamos a validar la cadena.
-	 * @return <i>true</i> en caso que la cadena recibida coincida con el patrón recibido, en caso contrario, retornamos <i>false</i>.
-	 */
-	public static boolean matchString(String string, String pattern) {
-		return (string == null) ? false : Pattern.matches(pattern, string.toLowerCase().trim());
-	}
 
 	/**
 	 * Se encarga de validar que los 2 elementos recibidos sean iguales. Permite recibir los 2 elementos nulos, lo que retorna que los elementos sean
--- trunk/src/test/java/com/common/util/tool/VerifierUtilUnitTest.java
+++ trunk/src/test/java/com/common/util/tool/VerifierUtilUnitTest.java
@@ -14,49 +14,6 @@ import org.junit.Test;
  * @version 1.0
  */
 public class VerifierUtilUnitTest {
-	/**
-	 * La pruebas sobre el método <i><b>isEmpty</b></i> de {@link VerifierUtil}
-	 * 
-	 * @see VerifierUtil#isEmpty(String)
-	 */
-	@Test
-	public void testIsEmpty() {
-
-		// Valor nulo.
-		Assert.assertEquals(true, VerifierUtil.isEmpty(null));
-
-		// Cadenas vacias o con espacios.
-		Assert.assertEquals(true, VerifierUtil.isEmpty(""));
-		Assert.assertEquals(false, VerifierUtil.isEmpty(" "));
-		Assert.assertEquals(false, VerifierUtil.isEmpty("  "));
-
-		// Cadenas con datos.
-		Assert.assertEquals(false, VerifierUtil.isEmpty("bob"));
-		Assert.assertEquals(false, VerifierUtil.isEmpty(" bob "));
-		Assert.assertEquals(false, VerifierUtil.isEmpty("  bob  "));
-	}
-
-	/**
-	 * La pruebas sobre el método <i><b>isBlank</b></i> de {@link VerifierUtil}
-	 * 
-	 * @see VerifierUtil#isBlank(String)
-	 */
-	@Test
-	public void testIsBlank() {
-
-		// Valor nulo.
-		Assert.assertEquals(true, VerifierUtil.isBlank(null));
-
-		// Cadenas vacias o con espacios.
-		Assert.assertEquals(true, VerifierUtil.isBlank(""));
-		Assert.assertEquals(true, VerifierUtil.isBlank(" "));
-		Assert.assertEquals(true, VerifierUtil.isBlank("  "));
-
-		// Cadenas con datos.
-		Assert.assertEquals(false, VerifierUtil.isBlank("bob"));
-		Assert.assertEquals(false, VerifierUtil.isBlank(" bob "));
-		Assert.assertEquals(false, VerifierUtil.isBlank("  bob  "));
-	}
 
 	/**
 	 * La pruebas sobre el método <i><b>isEqual</b></i> de {@link VerifierUtil}
@@ -67,26 +24,26 @@ public class VerifierUtilUnitTest {
 	public void testIsEqual() {
 
 		// Valores nulos.
-		Assert.assertEquals(true, VerifierUtil.<String>equals(null, null));
-		
+		Assert.assertEquals(true, VerifierUtil.<String> equals(null, null));
+
 		// Cadenas.
-		Assert.assertEquals(false, VerifierUtil.<String>equals(null, "bob"));
-		Assert.assertEquals(false, VerifierUtil.<String>equals("bob", null));
-		Assert.assertEquals(true, VerifierUtil.<String>equals("bob_1", "bob_1"));
-		Assert.assertEquals(false, VerifierUtil.<String>equals("bob_1", "bob_2"));
-		
+		Assert.assertEquals(false, VerifierUtil.<String> equals(null, "bob"));
+		Assert.assertEquals(false, VerifierUtil.<String> equals("bob", null));
+		Assert.assertEquals(true, VerifierUtil.<String> equals("bob_1", "bob_1"));
+		Assert.assertEquals(false, VerifierUtil.<String> equals("bob_1", "bob_2"));
+
 		// Enteros.
-		Assert.assertEquals(false, VerifierUtil.<Integer>equals(null, 10));
-		Assert.assertEquals(false, VerifierUtil.<Integer>equals(10, null));
-		Assert.assertEquals(true, VerifierUtil.<Integer>equals(10, 10));
-		Assert.assertEquals(false, VerifierUtil.<Integer>equals(10, 11));
-		
+		Assert.assertEquals(false, VerifierUtil.<Integer> equals(null, 10));
+		Assert.assertEquals(false, VerifierUtil.<Integer> equals(10, null));
+		Assert.assertEquals(true, VerifierUtil.<Integer> equals(10, 10));
+		Assert.assertEquals(false, VerifierUtil.<Integer> equals(10, 11));
+
 		// Dobles.
-		Assert.assertEquals(false, VerifierUtil.<Double>equals(null, 10.0));
-		Assert.assertEquals(false, VerifierUtil.<Double>equals(10.0, null));
-		Assert.assertEquals(true, VerifierUtil.<Double>equals(10.0, 10.0));
-		Assert.assertEquals(true, VerifierUtil.<Double>equals(10.5, 10.5));
-		Assert.assertEquals(false, VerifierUtil.<Double>equals(10.0, 11.0));
-		Assert.assertEquals(false, VerifierUtil.<Double>equals(10.5, 11.5));
+		Assert.assertEquals(false, VerifierUtil.<Double> equals(null, 10.0));
+		Assert.assertEquals(false, VerifierUtil.<Double> equals(10.0, null));
+		Assert.assertEquals(true, VerifierUtil.<Double> equals(10.0, 10.0));
+		Assert.assertEquals(true, VerifierUtil.<Double> equals(10.5, 10.5));
+		Assert.assertEquals(false, VerifierUtil.<Double> equals(10.0, 11.0));
+		Assert.assertEquals(false, VerifierUtil.<Double> equals(10.5, 11.5));
 	}
 }
