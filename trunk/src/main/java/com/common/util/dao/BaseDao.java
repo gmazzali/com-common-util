3dabb9c464f06daff6116f47d966b2cc2bec7be0
guillermomazzali@gmail.com <guillermomazzali@gmail.com@68265a98-090c-cc5e-7057-9da8e80d0a77>
2014-03-17 20:30:32 +0000

    Cambios sobre los DAOs y los Servicios base. Creación de un tipo de elemento "rango".

---
+++ trunk/src/main/java/com/common/util/dao/BaseDao.java
@@ -0,0 +1,190 @@
+package com.common.util.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.common.util.exception.UncheckedException;
+import com.common.util.model.Entity;
+import com.common.util.model.Persistence;
+import com.common.util.model.filter.BaseFilter;
+import com.common.util.model.query.filter.Filter;
+import com.common.util.model.query.order.OrderBy;
+
+/**
+ * La interfaz que define todos los métodos comunes a todos los DAOs que vamos a generar dentro de un sistema.
+ * 
+ * @see Entity
+ * @see Persistence
+ * @see Serializable
+ * 
+ * @since 05/02/2014
+ * @author Guillermo Mazzali
+ * @version 1.0
+ * 
+ * @param <E>
+ *            La clase que corresponde a la entidad que vamos a manipular dentro de este servicio.
+ * @param <PK>
+ *            La clase que corresponde al identificador de la entidad {@link E}.
+ */
+public abstract interface BaseDao<E extends Persistence<PK>, PK extends Serializable> extends Serializable {
+
+	/**
+	 * La función encargada de contar la cantidad de entidades.
+	 * 
+	 * @see #countByFilter(Filter)
+	 * 
+	 * @return El numero de registros.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la consulta de la cantidad de entidades.
+	 */
+	public Long count() throws UncheckedException;
+
+	/**
+	 * La función encargada de contar la cantidad de entidades que corresponden a un filtro del tipo {@link BaseFilter} que se recibe.
+	 * 
+	 * @see BaseFilter
+	 * 
+	 * @see #count()
+	 * 
+	 * @param filter
+	 *            El filtro del tipo {@link BaseFilter} con el que vamos a realizar la consulta de la cantidad de entidades que vamos a recuperar.
+	 * @return El numero de registros que va a corresponder a la consulta con el filtro.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la consulta de la cantidad de entidades.
+	 */
+	public Long countByFilter(BaseFilter<PK> filter) throws UncheckedException;
+
+	/**
+	 * La función que utilizamos para recuperar una entidad dado su identificador.
+	 * 
+	 * @see #findAll()
+	 * @see #findByFilter(Filter)
+	 * 
+	 * @param id
+	 *            El identificador de la entidad que vamos a recuperar desde la base de datos.
+	 * @return La entidad que corresponde al identificador recibido. En caso de no encontrar nada, retorna <i>NULL</i>.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la recuperación de la entidad desde la base de datos.
+	 */
+	public E findById(PK id) throws UncheckedException;
+
+	/**
+	 * La función que nos permite recuperar todos las entidades del mismo tipo almacenados dentro de la base de datos.
+	 * 
+	 * @see OrderBy
+	 * 
+	 * @see #findById(Serializable)
+	 * @see #findByFilter(Filter)
+	 * 
+	 * @param orders
+	 *            Los ordenes en los que queremos recuperar las entidades. Si el mismo es mulo, se recuperan si un orden en particular.
+	 * @return El listado de entidades almacenadas.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la recuperación de todos las entidades desde la base de datos.
+	 */
+	public List<E> findAll(OrderBy orders) throws UncheckedException;
+
+	/**
+	 * La función que utilizamos para recuperar una entidad dado un filtro del tipo {@link BaseFilter} para la consulta.
+	 * 
+	 * @see BaseFilter
+	 * @see OrderBy
+	 * 
+	 * @see #findAll()
+	 * @see #findById(Serializable)
+	 * 
+	 * @param filter
+	 *            El filtro del tipo {@link BaseFilter} que vamos a ocupar para recuperar un listado de entidades de acuerdo a un criterio.
+	 * @param orders
+	 *            Los ordenes dentro de {@link OrderBy} en los que queremos recuperar las entidades. Si el mismo es mulo, se recuperan si un orden en
+	 *            particular.
+	 * @return El listado de las entidades almacenadas que cumplen con la consulta recibida.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la recuperación de las entidades desde la base de datos.
+	 */
+	public List<E> findByFilter(BaseFilter<PK> filter, OrderBy orders) throws UncheckedException;
+
+	/**
+	 * La función para guardar una nueva entidad dentro de la base de datos.
+	 * 
+	 * @see Entity
+	 * 
+	 * @see #saveOrUpdate(Persistence)
+	 * @see #update(Persistence)
+	 * @see #delete(Persistence)
+	 * @see #deleteById(Serializable)
+	 * 
+	 * @param entity
+	 *            La entidad {@link E} que vamos a almacenar.
+	 * @return El identificador {@link PK} del elemento que acabamos de guardar dentro de la base de datos.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante el guardado de la entidad dentro de la base de datos.
+	 */
+	public PK save(E entity) throws UncheckedException;
+
+	/**
+	 * La función para actualizar una entidad dentro de la base de datos.
+	 * 
+	 * @see Entity
+	 * 
+	 * @see #save(Persistence)
+	 * @see #saveOrUpdate(Persistence)
+	 * @see #delete(Persistence)
+	 * @see #deleteById(Serializable)
+	 * 
+	 * @param entity
+	 *            La entidad {@link E} que vamos a actualizar.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la actualización de la entidad dentro de la base de datos.
+	 */
+	public void update(E entity) throws UncheckedException;
+
+	/**
+	 * La función para insertar una nueva entidad o actualizar una que ya se encuentre dentro de la base de datos.
+	 * 
+	 * @see Entity
+	 * 
+	 * @see #save(Persistence)
+	 * @see #update(Persistence)
+	 * @see #delete(Persistence)
+	 * @see #deleteById(Serializable)
+	 * 
+	 * @param entity
+	 *            La entidad {@link E} que vamos a insertar o actualizar.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la inserción o actualización de la entidad dentro de la base de datos.
+	 */
+	public void saveOrUpdate(E entity) throws UncheckedException;
+
+	/**
+	 * La función para eliminar una entidad dentro de la base de datos.
+	 * 
+	 * @see Entity
+	 * 
+	 * @see #save(Persistence)
+	 * @see #update(Persistence)
+	 * @see #saveOrUpdate(Persistence)
+	 * @see #deleteById(Serializable)
+	 * 
+	 * @param entity
+	 *            La entidad {@link E} que vamos a eliminar.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la eliminación de la entidad dentro de la base de datos.
+	 */
+	public void delete(E entity) throws UncheckedException;
+
+	/**
+	 * La función que nos permite eliminar una entidad de la base de datos solo dando su identificador.
+	 * 
+	 * @see #save(Persistence)
+	 * @see #update(Persistence)
+	 * @see #saveOrUpdate(Persistence)
+	 * @see #delete(Serializable)
+	 * 
+	 * @param id
+	 *            El identificador {@link PK} de la entidad que queremos eliminar de la base de datos.
+	 * @throws UncheckedException
+	 *             En caso de un problema durante la eliminación de la entidad dentro de la base de datos.
+	 */
+	public void deleteById(PK id) throws UncheckedException;
+}
---
+++ trunk/src/main/java/com/common/util/service/impl/BaseServiceImpl.java
@@ -0,0 +1,149 @@
+package com.common.util.service.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.common.util.dao.BaseDao;
+import com.common.util.exception.CheckedException;
+import com.common.util.model.Persistence;
+import com.common.util.model.filter.BaseFilter;
+import com.common.util.model.query.filter.Filter;
+import com.common.util.model.query.order.OrderBy;
+import com.common.util.service.BaseService;
+
+/**
+ * La clase que permite establecer un servicio para un elemento genérico junto al DAO correspondiente a este y que implementa la interfaz
+ * {@link BaseService}.
+ * 
+ * @see Filter
+ * @see OrderBy
+ * @see BaseService
+ * 
+ * @since 05/02/2014
+ * @author Guillermo Mazzali
+ * @version 1.0
+ * 
+ * @param <E>
+ *            La clase que corresponde a la entidad que vamos a manipular dentro de este servicio.
+ * @param <PK>
+ *            La clase que corresponde al identificador de la entidad {@link E}.
+ */
+public abstract class BaseServiceImpl<E extends Persistence<PK>, PK extends Serializable> implements BaseService<E, PK> {
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = Logger.getLogger(BaseServiceImpl.class);
+
+	/**
+	 * El DAO que permite manejar los elementos dentro de la base de datos.
+	 */
+	protected BaseDao<E, PK> dao;
+
+	@Override
+	public void setDao(BaseDao<E, PK> dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public Long count() throws CheckedException {
+		try {
+			return this.dao.count();
+		} catch (Exception e) {
+			log.error("count failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public Long countByFilter(BaseFilter<PK> filter) throws CheckedException {
+		try {
+			return this.dao.countByFilter(filter);
+		} catch (Exception e) {
+			log.error("count by filter failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public List<E> findAll(OrderBy orders) throws CheckedException {
+		try {
+			return this.dao.findAll(orders);
+		} catch (Exception e) {
+			log.error("find all failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public List<E> findByFilter(BaseFilter<PK> filter, OrderBy orders) throws CheckedException {
+		try {
+			return this.dao.findByFilter(filter, orders);
+		} catch (Exception e) {
+			log.error("find by filter failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public E findById(PK id) throws CheckedException {
+		try {
+			return this.dao.findById(id);
+		} catch (Exception e) {
+			log.error("find by id failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public void save(E entity) throws CheckedException {
+		try {
+			this.validate(entity);
+			this.dao.save(entity);
+		} catch (Exception e) {
+			log.error("save failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public void saveOrUpdate(E entity) throws CheckedException {
+		try {
+			this.validate(entity);
+			this.dao.saveOrUpdate(entity);
+		} catch (Exception e) {
+			log.error("save or update failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public void update(E entity) throws CheckedException {
+		try {
+			this.validate(entity);
+			this.dao.update(entity);
+		} catch (Exception e) {
+			log.error("update failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public void delete(E entity) throws CheckedException {
+		try {
+			this.dao.delete(entity);
+		} catch (Exception e) {
+			log.error("delete failed", e);
+			throw new CheckedException(e);
+		}
+	}
+
+	@Override
+	public void deleteById(PK id) throws CheckedException {
+		try {
+			this.dao.deleteById(id);
+		} catch (Exception e) {
+			log.error("delete by id failed", e);
+			throw new CheckedException(e);
+		}
+	}
+}
